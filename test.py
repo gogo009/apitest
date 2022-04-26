08ce351afe4f19dce03bc8bc2f662d00ae4ab7a4
gogo009 <88134665+gogo009@users.noreply.github.com>
2022-04-26 18:52:35 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+session = requests.session()
+
+# url = "http://newoa.800best.com"
+url = "http://oa.800best.ga/best/oa"
+
+headers = {"Cache-Control": "max-age=0", "Upgrade-Insecure-Requests": "1", "Origin": "http://oa.800best.ga", "Content-Type": "application/x-www-form-urlencoded"}
+
+data = {"username": "best_oa", "password": "Abc2467#123"}
+
+req = session.post(url, headers=headers, data=data)
+
+print(req.content)
