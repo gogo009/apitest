0a6eb6f1f996849f1bbaf26f2f8eeb035562d09e
gogo009 <88134665+gogo009@users.noreply.github.com>
2023-05-22 17:43:26 +0800

    del

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import requests
 session = requests.session()
 
 # url = "http://newoa.800best.com"
-url = "http://oa.800best.ga/best/oa"
+url = "http://oa.800best.life/best/oa"
 
-headers = {"Cache-Control": "max-age=0", "Upgrade-Insecure-Requests": "1", "Origin": "http://oa.800best.ga", "Content-Type": "application/x-www-form-urlencoded"}
+headers = {"Cache-Control": "max-age=0", "Upgrade-Insecure-Requests": "1", "Origin": "http://oa.800best.life", "Content-Type": "application/x-www-form-urlencoded"}
 
 data = {"username": "best_oa", "password": "Abc2467#123"}
 
